1c0669e374fc0f8290f5cfb68ff6e311bd6d9b5b
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-03-03 22:55:59 +0100

    Animation, thread, intent.

---
+++ app/src/main/java/com/example/manon/splashscreen/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.manon.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView iv;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        iv = findViewById(R.id.image);
+        tv = findViewById(R.id.welcomeText);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splashtransition);
+        iv.startAnimation(animation);
+        tv.startAnimation(animation);
+
+        final Intent i = new Intent(this, MainActivity.class);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
